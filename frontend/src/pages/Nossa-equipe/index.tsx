7fc4379305a9390ea594e2118314a48317ac1446
Rodrigoha <vinicius.a.prates2@gmail.com>
2026-04-16 15:16:59 -0300

    feat: css start

---
+++ frontend/src/pages/Nossa-equipe/index.tsx
@@ -0,0 +1,10 @@
+import { MenuItems } from '../../components/Menu-Items';
+import styles from './styless.module.css';
+
+export default function NossaEquipe() {
+  return (
+    <div className={styles.page}>
+      <MenuItems />
+    </div>
+  );
+}
