84ae7f969160230eb0599acc33541475950b34fa
Rodrigoha <vinicius.a.prates2@gmail.com>
2026-04-18 14:35:46 -0300

    feat: add banner

--- frontend/src/pages/Nossa-equipe/index.tsx
+++ frontend/src/pages/Nossa-equipe/index.tsx
@@ -1,10 +1,17 @@
 import { MenuItems } from '../../components/Menu-Items';
+import { Rectangle } from '../../components/Rectangle';
 import styles from './styles.module.css';
 
 export function NossaEquipe() {
   return (
     <div className={styles.page}>
-      <MenuItems />
+      <div className={styles.container}>
+        <MenuItems />
+        <Rectangle
+          title="Conheça nossa equipe"
+          text="Nosso time de jovens universitários apaixonados por tecnologia, empreendedorismo e inovação."
+        />
+      </div>
     </div>
   );
 }
