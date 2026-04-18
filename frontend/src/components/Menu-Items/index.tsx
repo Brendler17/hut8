cf102b2e2be3958aea83aefc4b35fcc46ac55d13
Brendler17 <gmbrendler@inf.ufpel.edu.br>
2026-04-18 10:48:28 -0300

    updated Menu-Items component

--- frontend/src/components/Menu-Items/index.tsx
+++ frontend/src/components/Menu-Items/index.tsx
@@ -1,21 +1,36 @@
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import logoHut from '../../assets/logo-hut.svg';
 import styles from './index.module.css';
 
 export function MenuItems() {
   return (
-    <div className={styles.menuItemsContainer}>
+    <header className={styles.menuItemsContainer}>
       <div className={styles.menuItemsLogo}>
         <img src={logoHut} alt="logotipo-hut" />
       </div>
-      <div className={styles.menuItemsLinks}>
-        <Link to="/">Home</Link>
-        <Link to="/projects">Projetos</Link>
-        <Link to="/team">Nossa equipe</Link>
-      </div>
+      <nav className={styles.menuItemsLinks}>
+        <NavLink
+          to="/"
+          className={({ isActive }) => (isActive ? styles.active : '')}
+        >
+          Home
+        </NavLink>
+        <NavLink
+          to="/projects"
+          className={({ isActive }) => (isActive ? styles.active : '')}
+        >
+          Projetos
+        </NavLink>
+        <NavLink
+          to="/team"
+          className={({ isActive }) => (isActive ? styles.active : '')}
+        >
+          Nossa equipe
+        </NavLink>
+      </nav>
       <div className={styles.menuItemsButton}>
         <button type="button">Entre pra hut</button>
       </div>
-    </div>
+    </header>
   );
 }
